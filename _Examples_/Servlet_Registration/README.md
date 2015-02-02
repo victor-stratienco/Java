10463e512e091969bc4193a87f03fe758bf95c8e
Victor Stratienco <victor.stratienco@phenogenomics.ca>
2015-02-02 11:33:47 -0500

    Example of Registration form in servlet

---
+++ _Examples_/Servlet_Registration/README.md
@@ -0,0 +1,2 @@
+# Example of Registration form in servlet
+http://www.javatpoint.com/example-of-registration-form-in-servlet
---
+++ _Examples_/Servlet_Registration/src/main/resources/database/db.sql
@@ -0,0 +1,10 @@
+----------------------
+-- oracle10g database
+----------------------
+
+CREATE TABLE  "REGISTERUSER"
+   (    "NAME" VARCHAR2(4000),
+    "PASS" VARCHAR2(4000),
+    "EMAIL" VARCHAR2(4000),
+    "COUNTRY" VARCHAR2(4000)
+   )
